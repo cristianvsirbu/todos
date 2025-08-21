20dfca923574e326f7d9515913dda298b2a9a263
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-08-21 14:33:54 +0300

    feat: add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./public/index.html"],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [],
+};
+
+export default config;
