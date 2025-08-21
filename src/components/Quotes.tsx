e39fa0f693a76ba43146639a7fdce07bc6658101
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-08-21 13:34:47 +0300

    feat: implement random quote fetching with error handling

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
 import { IoRefreshCircleOutline } from "react-icons/io5";
 
+const URL = "https://api.quotable.io/quotes/random";
+
+type Quote = {
+	author: string;
+	content: string;
+};
+
 const Quotes = () => {
+	const [quote, setQuote] = useState<Quote | null>(null);
+	const [error, setError] = useState<string | null>(null);
+
+	async function fetchQuote() {
+		setError(null);
+
+		try {
+			const result = await fetch(URL);
+			if (!result.ok) {
+				throw new Error(`HTTP error! status: ${result.status}`);
+			}
+			const data = await result.json();
+			setQuote(data[0]);
+		} catch (e) {
+			console.error(e);
+			setError("Failed to fetch quote!");
+		}
+	}
+
+	useEffect(() => {
+		fetchQuote();
+	}, []);
+
 	return (
 		<div>
 			<div className="flex flex-col items-center justify-center h-screen">
@@ -9,16 +40,23 @@ const Quotes = () => {
 					Inspire yourself with motivational quotes!
 				</p>
 				<div className="mt-8 p-4 bg-white shadow-md rounded-lg w-full max-w-lg">
-					<p className="text-xl font-semibold">
-						"The only limit to our realization of tomorrow is our doubts of
-						today."
-					</p>
-					<span className="text-sm text-gray-600 mt-2 float-end">
-						Franklin D. Roosevelt
-					</span>
+					{error && (
+						<p className="text-red-500 text-2xl font-semibold">{error}</p>
+					)}
+					{quote && !error && (
+						<>
+							<p className="text-xl font-semibold">{quote.content}</p>
+							<span className="text-sm text-gray-600 mt-2 float-end">
+								{quote.author}
+							</span>
+						</>
+					)}
 				</div>
 
-				<button className="cursor-pointer text-white mt-8 hover:scale-105 transition-transform duration-200">
+				<button
+					onClick={() => fetchQuote()}
+					className="cursor-pointer text-white mt-8 hover:scale-105 transition-transform duration-200"
+				>
 					<IoRefreshCircleOutline className="size-12 inline-block bg-blue-500 rounded-full hover:bg-blue-600 transition-colors duration-200" />
 				</button>
 			</div>
