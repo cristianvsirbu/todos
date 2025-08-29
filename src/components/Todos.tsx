81d73550fbaae33298aa3d0e619100b41e49a9e7
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-08-29 11:52:57 +0300

    fix: remove delete button from todo items in edit mode

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -257,14 +257,7 @@ const Todos = () => {
 							</div>
 							<div className="ml-auto flex items-center gap-2">
 								<span className="text-white font-semibold">{todo.urgency}</span>
-								{isEditMode && (
-									<button
-										onClick={() => handleDeleteTodo(todo.id)}
-										className="cursor-pointer hover:scale-105 transition-all duration-200 shadow-sm rounded-full"
-									>
-										<TbTrash className="size-8 p-1 text-white bg-red-500 rounded-full" />
-									</button>
-								)}
+								
 							</div>
 						</div>
 					))}
